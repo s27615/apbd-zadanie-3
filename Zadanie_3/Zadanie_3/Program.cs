66528d1925bf148e7fb0da020b28a09af2652672
Laura Klajn <s27615@pjwstk.edu.pl>
2024-03-19 13:22:42 +0100

    Added Classes

---
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Zadanie_3.Containers;
+
+Console.WriteLine("Hello, World!");
+
+//var container = new Container(10.0, 15.0);
+//{
+    // = 12.0,
+    //Weight = 12.0;
+//};
+//container.CargoWeight = 4.6;
+
+int? a = 1;
+a = null;
+Exception? ex = new Exception();
+ex = null;
+
+//ArraysList
+List<int> numbers = new List<int>() { 1, 2, 3 };
+List<int> numbers2 = new() { 1, 3, 4 };
+var numbers3 = new List<int>() { 4, 5, 6 };
+
+//HashMapa
+Dictionary<PossibleProducts, double> products = new();
+
+//Set
