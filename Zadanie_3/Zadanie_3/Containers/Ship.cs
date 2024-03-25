6f87c7d009b01cb40ac745a4c93815446ebc3d1a
s27615 <s27615@pjwstk.edu.pl>
2024-03-25 22:59:29 +0100

    class ship added and finished

---
+++ Zadanie_3/Zadanie_3/Containers/Ship.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml;
+
+namespace Zadanie_3.Containers;
+
+public abstract class Ship
+{
+    public Container[] Containers;
+    public double Speed;
+    public int MaxContainers;
+    public double MaxWeight;
+    protected Ship(int maxContainers, double speed, double maxWeight)
+    {
+        Containers = new Container[maxContainers];
+        Speed = speed;
+        MaxWeight = maxWeight;
+    }
+
+    public void show()
+    {
+        Console.WriteLine("Prędkość statku: " + Speed.ToString());
+        Console.WriteLine("Maksymalna waga wszystkich kontenerów: " + MaxWeight.ToString());
+        Console.WriteLine("Maksymalna liczba kontenerów na statku: " + MaxContainers.ToString());
+        showContainers();
+    }
+
+    public void showContainers()
+    {
+        Console.WriteLine("Na statku znajdują się: ");
+        foreach (Container c in Containers)
+        {
+            Console.WriteLine("Kontener " + c.NumberSer);
+        }
+
+    }
+}
--- Zadanie_3/Zadanie_3/Program.cs
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -1,29 +1,12 @@
 ﻿using Zadanie_3;
 using Zadanie_3.Containers;
 
-// var container = new Container(10.0, 15.0) 
-//     { 
-//         CargoWeight = 12.0,
-//         Height = 12.0
-//     };
-// container.CargoWeight = 10.0;
-
 Container c = new GasContainer(2.5, 90, 34, 4, 5);
 Container c1 = new GasContainer(2.5, 90, 34, 4, 5);
 Container c2 = new GasContainer(2.5, 90, 34, 4, 5);
 Console.WriteLine(c.NumberSer);
 Console.WriteLine(c1.NumberSer);
 Console.WriteLine(c2.NumberSer);
-
-
-int? a = 1;
-a = null;
-
-bool? b = true;
-b = null;
-
-Exception? ex = new Exception();
-ex = null;
 
 
 // ArrayList
