3c2e6230f3db12552cdfc3ace6b57cfa5648d25a
s27615 <s27615@pjwstk.edu.pl>
2024-03-25 22:06:59 +0100

    finished GasContainer

--- Zadanie_3/Zadanie_3/Containers/GasContainer.cs
+++ Zadanie_3/Zadanie_3/Containers/GasContainer.cs
@@ -1,6 +1,8 @@
-﻿namespace Zadanie_3.Containers;
+﻿using Zadanie_3.interfaces;
 
-public class GasContainer : Container
+namespace Zadanie_3.Containers;
+
+public class GasContainer : Container, IHazardNotifier
 {
     public GasContainer(double cargoWeight, double height, double weight, double depth, double max) : base(cargoWeight, height, weight, depth, max)
     {
@@ -10,5 +12,19 @@ public class GasContainer : Container
     public override void Load(PossibleProducts product, double cargoWeight)
     {
         base.Load(product, cargoWeight);
+        if (CargoWeight > MaxCargo)
+        {
+            throw new Exception("Przekroczono dopuszczalną łądowność");
+        }
+    }
+    public void Unload()
+    {
+        CargoWeight = 0.05 * CargoWeight;
+        Console.WriteLine("Opróżniono ładunek");
+    }
+
+    public void Notification(Container container)
+    {
+        Console.Write("Zaszła niebezpieczna sytuacja dla kontenera o numerze seryjnym: " + container.NumberSer);
     }
 }
